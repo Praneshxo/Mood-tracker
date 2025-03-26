5f4c925f6012e286ce07817b37e523e91e1d932d
Praneshxo <praneshgara@gmail.com>
2025-03-26 11:25:08 +0530

    11.25

---
+++ mood_tracker_webquest/src/Components/Hero.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../styles/Hero.css'
+
+function Hero() {
+    return (
+        <div className='hero'>
+                
+        </div>
+    )
+}
+
+export default Hero
---
+++ mood_tracker_webquest/src/Components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import '../styles/Navbar.css'
+
+function Navbar() {
+    return (
+     <div className='nav'>
+        <ul>
+            <li>home</li>
+            <li></li>
+        </ul>
+     </div>   
+    )
+}
+
+export default Navbar
